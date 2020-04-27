f161c7c49b8f7190cda61c728f31b791a492b5f4
huangfeidian <huangfeidian@live.cn>
2020-04-27 23:41:43 +0800

    add test

---
+++ test/dispatcher_test.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <iostream>
+#include <dispatcher.h>
+using namespace std;
+using namespace spiritsaway::dispatcher;
+
+void callback_1(const int& key, const int& value)
+{
+	cout << "callback_1 key " << key << " value " << value << endl;
+}
+void callback_2(int key, std::string value)
+{
+	cout << "callback_2 key " << key << " value " << value << endl;
+}
+void callback_3(const std::string& key, const std::string& value)
+{
+	cout << "callback_3 key " << key << " value " << value << endl;
+}
+void callback_4(std::string key, std::string value)
+{
+	cout << "callback_4 key " << key << " value " << value << endl;
+}
+class temp_a
+{
+public:
+	void callback_5(const std::string& key, const std::string& value)
+	{
+		cout << "callback_5 key " << key << " value " << value << endl;
+
+	}
+};
+int main()
+{
+	dispatcher<int, std::string> cur_dispatcher;
+	temp_a a;
+	auto handler_1 = cur_dispatcher.add_listener(1, &callback_1);
+	auto handler_2 = cur_dispatcher.add_listener<int, string>(2, &callback_2);
+	auto handler_3 = cur_dispatcher.add_listener(std::string("hehe"), &callback_3);
+	auto handler_4 = cur_dispatcher.add_listener<string, string>(std::string("1"), &callback_4);
+	auto handler_5 = cur_dispatcher.add_listener(std::string("1"), &temp_a::callback_5, &a);
+
+
+	cur_dispatcher.dispatch(1, 2);
+	cur_dispatcher.dispatch(2, string("hehe"));
+	cur_dispatcher.dispatch(std::string("1"), string("hehe"));
+	cur_dispatcher.dispatch(std::string("hehe"), string("hehe"));
+
+	cur_dispatcher.remove_listener(handler_5);
+	cur_dispatcher.remove_listener(handler_4);
+	cout << "after remove listener" << endl;
+	cur_dispatcher.dispatch(1, 2);
+	cur_dispatcher.dispatch(2, string("hehe"));
+	cur_dispatcher.dispatch(std::string("1"), string("hehe"));
+	cur_dispatcher.dispatch(std::string("hehe"), string("hehe"));
+
+	cur_dispatcher.remove_listener(handler_5);
+	cur_dispatcher.remove_listener(handler_4);
+
+}
+
